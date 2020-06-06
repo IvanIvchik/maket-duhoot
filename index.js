4ee8a45c7dbad4b08b73a8ad067ce2e584e95658
Ivan Ivchik <63555972+IvanIvchik@users.noreply.github.com>
2020-06-06 22:10:08 +0300

    Maket v1

---
+++ index.js
@@ -0,0 +1,9 @@
+﻿function hideMap() {
+    document.getElementById("hideMap").style.display = "none";
+    document.getElementById("showMap").style.display = "flex";
+}
+
+function showMap() {
+    document.getElementById("showMap").style.display = "none";
+    document.getElementById("hideMap").style.display = "flex";
+}
